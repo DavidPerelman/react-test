0b871bea5ec245cce268dcbb19091cdc6e38c443
David Perelman <dperelman3@gmail.com>
2022-11-21 18:11:25 +0200

    setup counter contains correct className test

--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -145,3 +145,43 @@ test('adding and then subtracting leads to the correct counter number', () => {
 
   expect(counterElement.textContent).toBe('15');
 });
+
+test('counter contains correct className', () => {
+  const component = render(<Counter />);
+  const counterElement = component.getByTestId('counter');
+  const inputElement = component.getByTestId('input');
+  const addBtnElement = component.getByTestId('add-btn');
+  const subtractBtnElement = component.getByTestId('subtract-btn');
+
+  expect(counterElement.className).toBe('');
+
+  fireEvent.change(inputElement, {
+    target: {
+      value: '50',
+    },
+  });
+
+  fireEvent.click(addBtnElement);
+
+  expect(counterElement.className).toBe('');
+
+  fireEvent.click(addBtnElement);
+
+  expect(counterElement.className).toBe('green');
+
+  fireEvent.click(addBtnElement);
+
+  expect(counterElement.className).toBe('green');
+
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+
+  expect(counterElement.className).toBe('');
+
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+  fireEvent.click(subtractBtnElement);
+
+  expect(counterElement.className).toBe('red');
+});
